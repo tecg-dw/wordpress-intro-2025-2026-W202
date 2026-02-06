b33b88e532fa738ebd821b458816bdadd1221a95
Dylan Jacquet <dylanjacquet235@gmail.com>
2026-02-06 12:26:57 +0100

    chore : Display acf fields

--- wp-content/themes/dw/front-page.php
+++ wp-content/themes/dw/front-page.php
@@ -1,19 +1,29 @@
-<!doctype html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
+<?php get_header(); ?>
 <h1>
   <?= get_the_title(); ?>
 </h1>
 <div>
   <?= get_the_content(); ?>
 </div>
-</body>
-</html>
+<a href="<?= get_the_permalink(); ?>" target="_blank" title="Lien vers cette page">
+  Lien vers cette page
+</a>
+<?php get_footer(); ?>
+
+<?php
+$title = get_field('title');
+$text = get_field('text');
+$image = get_field('background_image');
+?>
+
+<?php if ($title !== ''): ?>
+  <p><?= $title ?></p>
+<?php endif; ?>
+
+<div><?= $text ?></div>
+
+<img src="<?= $image['url'] ?>"
+     alt="<?= $image['alt'] ?>"
+     width="<?= $image['width'] ?>"
+     height="<?= $image['height'] ?>">
 
