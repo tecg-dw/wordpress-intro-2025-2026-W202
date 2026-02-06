3e349eb5d34d31c6774afe0dce93d467ae9be35d
Dylan Jacquet <dylanjacquet235@gmail.com>
2026-02-06 11:35:46 +0100

    fix : refactor code

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,19 +1,1 @@
 <?php
-
-// Gutenberg est le nouvel éditeur de contenu propre à Wordpress
-// il ne nous intéresse pas pour l'utilisation du thème que nous
-// allons créer. On va donc le désactiver :
-
-// Disable Gutenberg on the back end.
-add_filter( 'use_block_editor_for_post', '__return_false' );
-// Disable Gutenberg for widgets.
-add_filter( 'use_widgets_block_editor', '__return_false' );
-// Disable default front-end styles.
-add_action( 'wp_enqueue_scripts', function() {
-  // Remove CSS on the front end.
-  wp_dequeue_style( 'wp-block-library' );
-  // Remove Gutenberg theme.
-  wp_dequeue_style( 'wp-block-library-theme' );
-  // Remove inline global CSS on the front end.
-  wp_dequeue_style( 'global-styles' );
-}, 20 );
