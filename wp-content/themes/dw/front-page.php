2fe54d83414c56fc6e79f0a44946713971f20845
Dylan Jacquet <dylanjacquet235@gmail.com>
2026-02-13 10:46:38 +0100

    chore : Refactor front-page.php

--- wp-content/themes/dw/front-page.php
+++ wp-content/themes/dw/front-page.php
@@ -1,29 +1,5 @@
 <?php get_header(); ?>
-<h1>
-  <?= get_the_title(); ?>
-</h1>
-<div>
-  <?= get_the_content(); ?>
-</div>
-<a href="<?= get_the_permalink(); ?>" target="_blank" title="Lien vers cette page">
-  Lien vers cette page
-</a>
+
+<?php get_template_part( 'templates/components/stage/stage'); ?>
+
 <?php get_footer(); ?>
-
-<?php
-$title = get_field('title');
-$text = get_field('text');
-$image = get_field('background_image');
-?>
-
-<?php if ($title !== ''): ?>
-  <p><?= $title ?></p>
-<?php endif; ?>
-
-<div><?= $text ?></div>
-
-<img src="<?= $image['url'] ?>"
-     alt="<?= $image['alt'] ?>"
-     width="<?= $image['width'] ?>"
-     height="<?= $image['height'] ?>">
-
