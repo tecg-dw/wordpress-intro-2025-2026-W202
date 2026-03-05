2d200fab16984ea83471cd97139e2c50457acadd
Dylan Jacquet <dylanjacquet235@gmail.com>
2026-03-05 16:22:25 +0100

    feat: Add 404 page

---
+++ wp-content/themes/dw/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<section>
+  <h2>404 Page non trouvée</h2>
+  <p>Désolé, la page que vous recherchez n'existe pas ou a été déplacée.</p>
+  <p>Retour à <a href="/" title="Vers la page d'accueil">la page d'accueil</a> ou utilisez la recherche.</p>
+</section>
+
+<?php get_footer(); ?>
